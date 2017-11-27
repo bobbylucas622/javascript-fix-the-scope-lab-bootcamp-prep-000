152f4fe09d724c0a8cee12ba2f42e667cc705a73
Bobby Lucas <bobbylucas622@yahoo.com>
2017-11-27 06:03:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function add2(n) {
 }
 
 var theFunk = 'funky!'; 
-return funkyFunction.toUpperCase()                                  
+return theFunk.toUpperCase()                                  
 {
   
 }
