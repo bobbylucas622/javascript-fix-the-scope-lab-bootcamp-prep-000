70078835d86cb99d2c8b2a647a3d53f9ce5aadfe
Bobby Lucas <bobbylucas622@yahoo.com>
2017-11-27 06:05:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,9 +25,7 @@ return theFunk.toUpperCase()
   
 }
  
-  }
   
-}
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
